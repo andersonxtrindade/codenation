d29fdfb03caa8a86da37e6be862898acd1fd6268
Anderson Souza <axtrindade2012@yahoo.com>
2020-06-18 22:01:47 -0300

    Primeiros moduos

--- java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -6,18 +6,18 @@ import java.util.List;
 public class DesafioApplication {
 
 	public static List<Integer> fibonacci() {
-                List<Integer> res = new ArrayList<>();
-		res.add(0, 0);
-		res.add(1, 1);
-		int i = 2;
+                List<Integer> fibo = new ArrayList<>();
+		fibo.add(0, 0);
+		fibo.add(1, 1);
+		int k = 2;
 		int atual = 0;
 		while(atual < 350){
-			atual = (res.get(i-2) + res.get(i-1));
-			res.add(i,atual);
-			++i;
+			atual = (fibo.get(k-2) + fibo.get(k-1));
+			fibo.add(k,atual);
+			k+=1;
 		}
-		System.out.println(res);
-		return res;
+		System.out.println(fibo);
+		return fibo;
 	}
 
 	public static Boolean isFibonacci(Integer a) {
